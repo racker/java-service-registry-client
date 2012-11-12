5f66d270eb8e54671063681017edd7f1400ceade
gdusbabek <gdusbabek@gmail.com>
2012-11-12 09:49:09 -0600

    com.rackspacecloud.client.service_registry -> com.rackspacecloud.service_registry.client

---
+++ src/main/java/com/rackspacecloud/service_registry/client/service_registry/Region.java
@@ -0,0 +1,6 @@
+package com.rackspacecloud.service_registry.client.service_registry;
+
+public class Region {
+    public static final String US = "us";
+    public static final String UK = "uk";
+}
