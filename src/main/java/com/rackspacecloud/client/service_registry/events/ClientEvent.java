fef79057e8f730b3075ae3b89000b0a09a3748f7
gdusbabek <gdusbabek@gmail.com>
2013-01-15 11:11:49 -0600

    brace in wrong place.

--- src/main/java/com/rackspacecloud/client/service_registry/events/ClientEvent.java
+++ src/main/java/com/rackspacecloud/client/service_registry/events/ClientEvent.java
@@ -2,8 +2,7 @@ package com.rackspacecloud.client.service_registry.events;
 
 import java.util.EventObject;
 
-public class ClientEvent extends EventObject 
-{
+public class ClientEvent extends EventObject {
     private final int httpStatus;
     
     public ClientEvent(Object source, int httpStatus) {
