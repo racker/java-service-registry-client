6edbba3cccc6051f5b3f587ca914ece13b6ba456
gdusbabek <gdusbabek@gmail.com>
2013-01-15 11:12:58 -0600

    comment.

--- src/main/java/com/rackspacecloud/client/service_registry/events/ClientEventThread.java
+++ src/main/java/com/rackspacecloud/client/service_registry/events/ClientEventThread.java
@@ -4,6 +4,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ * This singleton contains the thread that will process and emit client events.  Right now it is limited to one thread
+ * to ensure a serial property.
+ */
 public class ClientEventThread {
     private static final ExecutorService workers = Executors.newSingleThreadExecutor();
     
